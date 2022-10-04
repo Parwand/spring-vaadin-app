fd3b25d47fbd75e03fcb746b3e4166c3f1923748
Parwand Alsino <parwand.alsino@hhu.de>
2022-10-04 17:13:46 +0200

    created TodoView

--- src/main/java/com/parwand/application/views/todo/ToDoView.java
+++ src/main/java/com/parwand/application/views/todo/ToDoView.java
@@ -1,2 +1,30 @@
-package com.parwand.application.views.todo;public class ToDoView {
+package com.parwand.application.views.todo;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route("/todo")
+public class ToDoView extends VerticalLayout {
+
+    public ToDoView() {
+        VerticalLayout todoList = new VerticalLayout();
+        TextField todoField = new TextField("Todo");
+        Button addButton = new Button("Add Todo to List");
+
+        addButton.addClickListener(e -> {
+            Checkbox checkbox = new Checkbox(todoField.getValue());
+            todoList.add(checkbox);
+        });
+
+        HorizontalLayout hl = new HorizontalLayout(todoField, addButton);
+        hl.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+
+        add(new H1("Vaadin Todos"),
+                todoList, hl);
+    }
 }
