712a11f48a6808d1d48e66b1dece1dde5086e6a2
Parwand Alsino <parwand.alsino@hhu.de>
2022-10-04 14:25:53 +0200

    changed components structure

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -4,29 +4,26 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Main")
 @Route(value = "")
-public class MainView extends HorizontalLayout {
-
-    private TextField name;
-    private Button sayHello;
+public class MainView extends VerticalLayout {
+    TextField name;
+    Button sayHello;
 
     public MainView() {
         name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
+        sayHello = new Button("Click me");
+
+        sayHello.addClickListener(e -> Notification.show(name.getValue()));
         sayHello.addClickShortcut(Key.ENTER);
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        HorizontalLayout hl = new HorizontalLayout(name, sayHello);
+        hl.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+        add(hl);
     }
 
 }
