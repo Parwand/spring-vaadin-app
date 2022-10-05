70ccc84b903b614e9c815a5a6bb08f1ccc60df9f
Parwand Alsino <parwand.alsino@hhu.de>
2022-10-05 09:56:28 +0200

    created Drawer with todo view

--- src/main/java/com/parwand/application/views/main/MainLayout.java
+++ src/main/java/com/parwand/application/views/main/MainLayout.java
@@ -1,18 +1,27 @@
 package com.parwand.application.views.main;
 
+import com.parwand.application.views.todo.ToDoView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.RouterLink;
 
 public class MainLayout extends AppLayout {
 
     public MainLayout() {
         createHeader();
+        createDrawer();
     }
 
     private void createHeader() {
         HorizontalLayout header = new HorizontalLayout(new DrawerToggle());
         header.setSizeFull();
         addToNavbar(header);
+    }
+
+    private void createDrawer() {
+        RouterLink todo = new RouterLink("TODO", ToDoView.class);
+        todo.setHighlightCondition(HighlightConditions.sameLocation());
     }
 }
